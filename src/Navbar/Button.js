7b94d86141eb907819355404d4acceceab04fe60
MasaKomazec <komazecevaa@gmail.com>
2021-08-27 19:11:47 +0200

    dugme Like

---
+++ src/Navbar/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+export const Button = ({
+    children,
+    onClick,
+}) => {
+    return (
+        <button className={`btn 'btn--primary' 'btn--medium'`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
