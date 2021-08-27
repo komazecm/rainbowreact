03f45072ed3fa16d29085a322b7f5f726686cea2
MasaKomazec <komazecevaa@gmail.com>
2021-08-27 19:25:00 +0200

    navbar i menuItems dodati i stilizovani

---
+++ src/Navbar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        className: 'nav-links'
+    },
+    {
+        title: 'Flashcards',
+        url: '/flashcards',
+        className: 'nav-links'
+    }
+]
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { MenuItems } from "./MenuItems";
+import { Button } from "./Button";
+import { Route, Link } from 'react-router-dom';
+import './Navbar.css';
+
+class Navbar extends React.Component {
+    state = { 
+        clicked: false,
+    }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    like() {
+        alert("Sviđa vam se ova stranica!");
+    }
+
+    render() {
+        return (
+            <nav className="NavItems">
+                <h1 className="logo"><Link to="/" className="logo">Rainbow School</Link></h1>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li className={item.className}>
+                                <Link to={item.url}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Button onClick={this.like}>Like</Button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
