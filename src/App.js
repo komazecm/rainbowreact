4c1d5863870654379c90e1d90850a99182742f72
MasaKomazec <komazecevaa@gmail.com>
2021-08-28 13:38:50 +0200

    dodata komponenta Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './Navbar/Navbar';
 import Home from './Home';
 import {Route, Link} from 'react-router-dom';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Route exact path="/" component={Home}/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+
+const data = {
+    email: "office@learnenglish.com",
+    phone: "+38111263565",
+    address: "Milana Bartoša 20, Beograd"
+}
+
+class Footer extends React.Component {
+    
+    render() {
+        return (
+            <div className="footer-container">
+                <h1 className="footer-info">Kontakt</h1>
+                <ul className="list-items">
+                    <li>{data.email}</li>
+                    <li>{data.phone}</li>
+                    <li>{data.address}</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Footer;
