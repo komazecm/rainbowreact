6b17b5224908531f10f7f5bc18914e648c4ee614
MasaKomazec <komazecevaa@gmail.com>
2021-08-29 13:39:45 +0200

    dodate slike

--- src/Images.js
+++ src/Images.js
@@ -1,34 +1,34 @@
 export const Images = [
     {
-        url: '',
+        url: 'https://iecc.rs/wp-content/uploads/2019/03/Ucionice-rainbow-1-1.jpg',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://iecc.rs/wp-content/uploads/2019/03/RAIBOW-PRIPREMNI-CENTAR.jpg.webp',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://iecc.rs/wp-content/uploads/2019/03/ucionice-rainbow-2.jpg.webp',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://cdn.navidiku.rs/firme/galerija/s6870/male/slike-skola-engleskog-jezika-rainbow129748.jpg',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Zps-exd2iQuqZt0YYYRimXaDk5vQOkRZSQ&usqp=CAU',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://cdn.navidiku.rs/firme/proizvodgalerija/galerija22069/velike/skola-engleskog-za-decu-resnik.jpg',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://cdn.navidiku.rs/firme/proizvodgalerija/galerija22073/velike/skola-stranih-jezika-resnik.jpg',
         className: 'img'
     },
     {
-        url: '',
+        url: 'https://cdn.navidiku.rs/firme/proizvodgalerija/galerija22070/male6/skola-jezika-miljakovac.jpg',
         className: 'img'
     }
 ]
